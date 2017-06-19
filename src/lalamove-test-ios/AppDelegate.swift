54458c09c58edc17a009430234c12dfa356c7df3
Derek Cheung <derekthcheung@gmail.com>
2017-06-19 17:54:23 +0800

    project setup

---
+++ src/lalamove-test-ios/AppDelegate.swift
@@ -0,0 +1,68 @@
+//
+//  AppDelegate.swift
+//  lalamove-test-ios
+//
+//  Created by Derek Cheung on 19/6/2017.
+//  Copyright © 2017 Derek Cheung. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	// MARK: - Variables
+
+	var window: UIWindow?
+
+	var _navigationController: UINavigationController?
+	var navigationController: UINavigationController! {
+
+		get {
+
+			if (_navigationController == nil) {
+
+				_navigationController = UINavigationController()
+			}
+
+			 return _navigationController
+		}
+	}
+
+	// MARK: - <UIApplicationDelegate>
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+		self.setupLandingScreen()
+
+		return true
+	}
+
+	func applicationWillResignActive(_ application: UIApplication) {
+	}
+
+	func applicationDidEnterBackground(_ application: UIApplication) {
+	}
+
+	func applicationWillEnterForeground(_ application: UIApplication) {
+	}
+
+	func applicationDidBecomeActive(_ application: UIApplication) {
+	}
+
+	func applicationWillTerminate(_ application: UIApplication) {
+	}
+
+	// MARK: - Helpers
+
+	func setupLandingScreen() {
+
+		self.window = UIWindow.init(frame: UIScreen.main.bounds)
+		self.window?.makeKeyAndVisible()
+
+		self.window?.rootViewController = self.navigationController
+
+		self.navigationController.viewControllers = [ ViewController() ]
+	}
+}
+
---
+++ src/lalamove-test-ios/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  lalamove-test-ios
+//
+//  Created by Derek Cheung on 19/6/2017.
+//  Copyright © 2017 Derek Cheung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	// MARK: - View lifecycle
+
+	override func loadView() {
+
+		super.loadView()
+
+		self.view = UIView()
+		self.view.backgroundColor = UIColor.blue
+	}
+
+	override func viewDidLoad() {
+
+		super.viewDidLoad()
+	}
+}
+
